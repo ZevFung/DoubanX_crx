da508d39ba8f9f9567dd93d3146fc1eb2a538fd3
wange <i@wange.im>
2016-03-19 00:12:38 +0800

    为避免和页面中的变量名冲突，重命名变量

--- js/view/template.js
+++ js/view/template.js
@@ -17,11 +17,11 @@ class Template {
      */
     typeA() {
         const data = this.data;
-        $("body").on("click", "#interest_sectl .close", function(ev) {
+        $("body").on("click", "#interest_sectl .interest_close", function(ev) {
             $("#interest_sectl").remove();
         });
         return `<div id="interest_sectl">
-                    <a href="javascript:;" class="close"></a>
+                    <a href="javascript:;" class="interest_close"></a>
                     <div class="rating_wrap clearbox">
                         <div class="rating_logo">豆瓣评分</div>
                         <div class="rating_self clearfix">
@@ -29,7 +29,7 @@ class Template {
                             <div class="rating_right">
                                 <div class="ll bigstar${data.star}"></div>
                                 <div class="rating_sum">
-                                    <a href="https://${data.type}.douban.com/subject/${data.id}/collections" class="rating_people"><span>${data.vote}</span>人评价</a>
+                                    <a href="https://${data.type}.douban.com/subject/${data.id}/collections" class="rating_people" target="_blank"><span>${data.vote}</span>人评价</a>
                                 </div>
                             </div>
                         </div>
