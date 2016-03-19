69d257793ded09655c60a2b3aa06e43e30a4058c
wange <i@wange.im>
2016-03-19 08:51:05 +0800

    增加对乐视视频的支持

---
+++ js/controller/leshi.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class Leshi {
+    constructor() {
+        this.isLeshi = window.location.host === 'www.le.com';
+        this.page = {
+            video: window.location.pathname.indexOf('/ptv/vplay/') === 0
+        }
+    }
+
+    main() {
+        if (this.isLeshi && this.page.video) {
+            new DoubanX({
+                name: $('h1.j-video-name').text(),
+                type: 'movie'
+            }).getRate();
+        }
+
+    }
+}
+
+new Leshi().main();
