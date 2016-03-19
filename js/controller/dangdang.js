a905ca25a8118b7f95ca83b274b8b35145151932
wange <i@wange.im>
2016-03-19 08:56:33 +0800

    增加对当当图书的支持

---
+++ js/controller/dangdang.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class Dangdang {
+    constructor() {
+        this.isDangdang = window.location.host === 'product.dangdang.com';
+        this.page = {
+            book: $.trim($('.breadcrumb a').eq(0).text()) === '图书'
+        }
+    }
+
+    main() {
+        if (this.isDangdang && this.page.book) {
+            new DoubanX({
+                name: $('.name_info h1').text(),
+                type: 'book'
+            }).getRate();
+        }
+
+    }
+}
+
+new Dangdang().main();
