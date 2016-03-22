a5f642143816eb678c5287c57a9d0798c43687cd
wange <i@wange.im>
2016-03-22 11:03:04 +0800

    增加对京东图书的支持

---
+++ js/controller/jd.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class JD {
+    constructor() {
+        this.isJD = window.location.host === 'item.jd.com';
+        this.page = {
+            book: $.trim($('.breadcrumb strong a').text()) === '图书'
+        }
+    }
+
+    main() {
+        if (this.isJD && this.page.book) {
+            new DoubanX({
+                name: $('#name h1').text(),
+                type: 'book'
+            }).getRate();
+        }
+
+    }
+}
+
+new JD().main();
