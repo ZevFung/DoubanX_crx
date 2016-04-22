ae2af8e0a15759ddb84bd5b04ae6046a8707ea7c
wange <i@wange.im>
2016-04-22 09:55:23 +0800

    支持京东电子书

--- js/controller/jd.js
+++ js/controller/jd.js
@@ -1,9 +1,14 @@
 class JD {
     constructor() {
+        // 京图图书
         this.isJD = (window.location.host === 'item.jd.com') &&
                     (document.querySelector('.breadcrumb strong a').innerText.trim() === '图书');
+        // 京图热卖
         this.isRe = (window.location.host === 're.jd.com') &&
                     (window.location.pathname.indexOf('/cps/item/') === 0);
+        // 京图电子书
+        this.isE = (window.location.host === 'e.jd.com') &&
+                    (document.querySelector('#name h2'))
     }
 
     main() {
@@ -17,6 +22,13 @@ class JD {
         if (this.isRe) {
             new DoubanX({
                 name: document.querySelector('.shop_intro h2 a').innerText,
+                type: 'book'
+            }).getRate();
+        }
+
+        if (this.isE) {
+            new DoubanX({
+                name: document.querySelector('#name h2').innerText,
                 type: 'book'
             }).getRate();
         }
