fc1c5a96b3aad061bb44abcdc894e02ec4d27ec7
wange <i@wange.im>
2016-04-22 09:47:54 +0800

    支持京东热卖上的图书

--- js/controller/jd.js
+++ js/controller/jd.js
@@ -1,19 +1,25 @@
 class JD {
     constructor() {
-        this.isJD = window.location.host === 'item.jd.com';
-        this.page = {
-            book: document.querySelector('.breadcrumb strong a').innerText.trim() === '图书'
-        };
+        this.isJD = (window.location.host === 'item.jd.com') &&
+                    (document.querySelector('.breadcrumb strong a').innerText.trim() === '图书');
+        this.isRe = (window.location.host === 're.jd.com') &&
+                    (window.location.pathname.indexOf('/cps/item/') === 0);
     }
 
     main() {
-        if (this.isJD && this.page.book) {
+        if (this.isJD) {
             new DoubanX({
                 name: document.querySelector('#name h1').innerText,
                 type: 'book'
             }).getRate();
         }
 
+        if (this.isRe) {
+            new DoubanX({
+                name: document.querySelector('.shop_intro h2 a').innerText,
+                type: 'book'
+            }).getRate();
+        }
     }
 }
 
