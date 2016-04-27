bdcc2c44cd6490002050c55fe52346310b2b7de9
wange <i@wange.im>
2016-04-27 14:00:12 +0800

    增加jd对https的支持

--- js/controller/jd.js
+++ js/controller/jd.js
@@ -2,33 +2,33 @@ class JD {
     constructor() {
         // 京图图书
         this.isJD = (window.location.host === 'item.jd.com') &&
-                    (document.querySelector('.breadcrumb strong a').innerText.trim() === '图书');
+                    ($.trim($('.breadcrumb strong a').text()) === '图书');
         // 京图热卖
         this.isRe = (window.location.host === 're.jd.com') &&
                     (window.location.pathname.indexOf('/cps/item/') === 0);
         // 京图电子书
         this.isE = (window.location.host === 'e.jd.com') &&
-                    (document.querySelectorAll('#name h2').length > 0)
+                    ($('#name h2').length > 0);
     }
 
     main() {
         if (this.isJD) {
             new DoubanX({
-                name: document.querySelector('#name h1').innerText,
+                name: $('#name h1').text(),
                 type: 'book'
             }).getRate();
         }
 
         if (this.isRe) {
             new DoubanX({
-                name: document.querySelector('.shop_intro h2 a').innerText,
+                name: $('.shop_intro h2 a').text(),
                 type: 'book'
             }).getRate();
         }
 
         if (this.isE) {
             new DoubanX({
-                name: document.querySelector('#name h2').innerText,
+                name: $('#name h2').text(),
                 type: 'book'
             }).getRate();
         }
