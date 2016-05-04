d149a31998eec620eff2fe8329505ad36bd25722
wange <i@wange.im>
2016-05-04 13:07:47 +0800

    修正匹配精度

--- js/controller/douban.js
+++ js/controller/douban.js
@@ -5,12 +5,12 @@ class Douban {
     main() {
         Common.listHandle(
             /^https:\/\/movie\.douban\.com\/subject\/\d*/i,
-            '.recommendations-bd dl, .list-s li',
+            '.recommendations-bd dl, .list-s li, #billboard tr',
             'movie'
         );
         Common.listHandle(
             /(^https:\/\/(book|read)\.douban\.com)*\/(subject|ebook)\/\d*/i,
-            '#db-rec-section dl, #rec-ebook-section dl, .article-recommends li',
+            '#db-rec-section dl, #rec-ebook-section dl, .article-recommends li, .list-ranking li, #book_rec dl, .slide-list li, .rankings ul.list li, .popular-works li',
             'book'
         );
     }
